8c8e407ad552293db19412454b80e19720812e42
Vincent DELMAESTRO <vincent.delmaestro@yahoo.com>
2021-09-09 19:14:13 +0200

    Initial Commit

---
+++ Habits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habits
+//
+//  Created by Vincent Delmaestro on 09/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Vincent Delmaestro on 09/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
