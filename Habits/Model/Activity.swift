5ac6ecedd0937d8751a80dfabb5a4b1c71e1d634
Vincent DELMAESTRO <vincent.delmaestro@yahoo.com>
2021-09-09 19:19:45 +0200

    feature: Create Activity struct

---
+++ Habits/Model/Activity.swift
@@ -0,0 +1,13 @@
+//
+//  Activity.swift
+//  Habits
+//
+//  Created by Vincent Delmaestro on 09/09/2021.
+//
+
+import Foundation
+
+struct Activity {
+	var title: String
+	var description: String
+}
